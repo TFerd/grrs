95710e548a3ee2e6d9f1bd5848458cebc0bc27d9
Thomas Ferdinand <thomasferdinand96@gmail.com>
2025-08-15 21:55:25 -0400

    adding directories to queue only if recursion is enabled and removing an else block. also using write buffer to log instead of print

--- src/main.rs
+++ src/main.rs
@@ -59,59 +59,38 @@ fn main() {
 
         if path.is_dir() {
             log(format!("Path {:?} is a directory", path), verbose);
-            if recurse == true {
-                let mut queue = VecDeque::<PathBuf>::new();
-                queue.push_back(path.to_path_buf());
-                while !queue.is_empty() {
-                    let next_dir = queue.pop_front().unwrap();
+            let mut queue = VecDeque::<PathBuf>::new();
+            queue.push_back(path.to_path_buf());
+            while !queue.is_empty() {
+                let next_dir = queue.pop_front().unwrap();
 
-                    log(format!("Checking {:?}", next_dir.as_path()), verbose);
+                log(format!("Checking {:?}", next_dir.as_path()), verbose);
 
-                    for next_dir_item in next_dir.read_dir().unwrap() {
-                        let next_dir_item_path = next_dir_item.unwrap().path();
+                for next_dir_item in next_dir.read_dir().unwrap() {
+                    let next_dir_item_path = next_dir_item.unwrap().path();
 
-                        log(format!("Checking {:?}", next_dir_item_path), verbose);
+                    log(format!("Checking {:?}", next_dir_item_path), verbose);
 
-                        if next_dir_item_path.is_dir() {
+                    if next_dir_item_path.is_dir() {
+                        if recurse == true {
                             queue.push_back(next_dir_item_path.to_path_buf());
-                        } else {
-                            let content = &read_to_string(next_dir_item_path).unwrap_or_default();
+                        }
+                    } else {
+                        let content = &read_to_string(next_dir_item_path).unwrap_or_default();
 
-                            match output_file {
-                                Some(ref mut x) => {
-                                    log(format!("Writing to file {:?}", x), verbose);
-                                    let vec = grrs::return_matches(&search_query, content);
+                        match output_file {
+                            Some(ref mut x) => {
+                                log(format!("Writing to file {:?}", x), verbose);
+                                let vec = grrs::return_matches(&search_query, content);
 
-                                    for line in vec {
-                                        x.write_all(line.as_bytes()).unwrap();
-                                    }
-                                }
-                                None => {
-                                    grrs::find_matches(&search_query, content, &std::io::stdout())
-                                        .unwrap();
+                                for line in vec {
+                                    x.write_all(line.as_bytes()).unwrap();
                                 }
                             }
-                        }
-                    }
-                }
-            } else {
-                let mut contents = path.read_dir().unwrap();
-                while let Some(content) = contents.next() {
-                    let content = read_to_string(content.unwrap().path()).unwrap_or("".to_string());
-
-                    match output_file {
-                        Some(ref mut x) => {
-                            log(format!("Writing to file {:?}", x), verbose);
-                            let vec = grrs::return_matches(&search_query, &content);
-
-                            for line in vec {
-                                x.write_all(line.as_bytes()).unwrap();
-                                x.write_all(b"\n").unwrap();
+                            None => {
+                                grrs::find_matches(&search_query, content, &std::io::stdout())
+                                    .unwrap();
                             }
-                        }
-                        None => {
-                            grrs::find_matches(&search_query, &content, &std::io::stdout())
-                                .unwrap();
                         }
                     }
                 }
@@ -150,5 +129,5 @@ fn log(message: String, verbose: bool) {
         return;
     }
 
-    println!("{}", message);
+    write!(&std::io::stdout(), "{}", message).unwrap();
 }
